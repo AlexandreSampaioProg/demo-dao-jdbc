c6780dd11ceb0e68ff0e72c9f3d8e0d6c1bb6a4c
Alexandre Sampaio <alexandresampaioprog@gmail.com>
2024-08-09 11:58:09 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,19 @@
 package application;
 
+import java.util.Date;
 import model.entities.Departament;
+import model.entities.Seller;
 
 public class Program {
 
     public static void main(String[] args) {
         Departament obj = new Departament (1, "Books");
+        
+        
+        Seller seller = new Seller(1, "juao", "asas", new Date(), 1000, obj);
+            
         System.out.println(obj);
+        System.out.println(seller);
+        
     }
-    
 }
