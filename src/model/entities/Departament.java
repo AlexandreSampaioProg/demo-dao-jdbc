854180173588546473c93e076b5951d1d2551e8d
Alexandre Sampaio <alexandresampaioprog@gmail.com>
2024-08-09 11:19:47 -0300

    Department entity class

---
+++ src/model/entities/Departament.java
@@ -0,0 +1,64 @@
+
+package model.entities;
+
+import java.io.Serializable;
+
+public class Departament implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+
+    private int id;
+    private String name;
+
+    public Departament() {
+    }
+
+    public Departament(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override                           //hashCode is important to compare ids content instead of memory code 
+    public int hashCode() {
+        int hash = 3;
+        hash = 83 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Departament other = (Departament) obj;
+        return this.id == other.id;
+    }
+
+    @Override
+    public String toString() {
+        return "Departament{" + "id=" + id + ", name=" + name + '}';
+    }
+  
+}
